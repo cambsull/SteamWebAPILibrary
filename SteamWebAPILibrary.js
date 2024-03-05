7fc6483b77117de21803ee0710753acd126fa272
Cambria Sullivan <72573258+cambsull@users.noreply.github.com>
2024-03-04 18:49:52 -0800

    updated README, added methods, updated .gitignore

--- SteamWebAPILibrary.js
+++ SteamWebAPILibrary.js
@@ -21,8 +21,7 @@ class CallSteamAPI {
             .catch(error => console.error('The server returned an error. Did you specify a valid game id? ', error))
     }
     async getPlayerSummaries(steamids) {
-        const url = `${this.baseURL}/ISteamUser/GetPlayerSummaries/v002/?key=${this.key}&steamids=${steamids}`;
-
+        const url = `${this.baseURL}/ISteamUser/GetPlayerSummaries/v0002/?key=${this.key}&steamids=${steamids}`;
         await fetch(url)
             .then(response => response.json())
             .then(data => {
@@ -32,7 +31,46 @@ class CallSteamAPI {
             })
             .catch(error => console.error('The server returned an error. Did you specify a valid list of Steam IDs in ONE string, separated by commas? ', error))
     }
-}
+    async getFriendList(steamid, relationship=`friend`){
+        const url = `${this.baseURL}/ISteamUser/GetFriendList/v0001/?key=${this.key}&steamid=${steamid}&relationship=${relationship}`;
+        await fetch(url)
+            .then(response => response.json())
+            .then(data => console.log(data.friendslist.friends))
+            .catch(error => console.error('The server returned an error. Did you specify a valid steam id? ', error))
+    }
+    async getPlayerAchievements(steamid, appid) {
+        const url = `${this.baseURL}/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${appid}&key=${this.key}&steamid=${steamid}`;
+        await fetch(url)
+            .then(response => response.json())
+            .then(data => console.log(data.playerstats))
+            .catch(error => console.error('The server returned an error. Did you specify a valid Steam ID and app ID? ', error))
+    }
+    async getUserStatsForGame(steamid, appid) {
+        const url = `${this.baseURL}/ISteamUserStats/GetUserStatsForGame/v0002/?appid=${appid}&key=${this.key}&steamid=${steamid}`;
+        await fetch(url)
+            .then(response => response.json())
+            .then(data => console.log(data.playerstats))
+            .catch(error => console.error('The server returned an error. Did you specify a valid Steam ID and app ID? ', error))
+    }
+    async getOwnedGames(steamid, includeAppinfo=false, includePlayedFreeGames=false) {
+        const includeAppInfoParam = includeAppinfo ? `&include_appinfo=true&` : '';
+        const includePlayedFreeGamesParam = includePlayedFreeGames ? `&include_played_free_games=true&` : '';
 
+        const url = `${this.baseURL}/IPlayerService/GetOwnedGames/v0001/?key=${this.key}&steamid=${steamid}${includeAppInfoParam}${includePlayedFreeGamesParam}&format=json`;
+        await fetch(url)
+            .then(response => response.json())
+            .then(data => data.response.games.forEach(game => {console.log(game)}))
+            .catch(error => console.error('The server returned an error. Did you specify a valid Steam ID? ', error))
+    }
+    async getRecentlyPlayedGames(steamid, count=null) {
+        const countParam = count ? `&count=${count}` : '';
+
+        const url = `${this.baseURL}/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${this.key}&steamid=${steamid}${countParam}&format=json`;
+        await fetch(url)
+            .then(response => response.json())
+            .then(data => console.log(data.response))
+            .catch(error => console.error('The server returned an error. Did you specify a valid Steam ID? ', error))
+    }
+}
 
 export default CallSteamAPI;
